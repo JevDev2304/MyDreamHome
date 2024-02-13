e0bb63872cfc12bbab5c1567067ce9ab19144758
Wilson <wilson.santiago.car@gmail.com>
2024-02-12 19:54:07 -0500

    pequennio avance colocar logos en mapa

--- src/app/module-maps/components/map-view/map-view.component.ts
+++ src/app/module-maps/components/map-view/map-view.component.ts
@@ -4,6 +4,7 @@ import { Map , Popup, Marker, LngLatLike} from 'mapbox-gl';
 import { MainComponent } from '../../../main/main.component';
 import { HousesService } from '../../../services/houses.service';
 import { HobbiesService } from '../../../services/hobbies.service';
+import { CategoriesService } from '../../../services/categories.service';
 
 @Component({
   selector: 'app-map-view',
@@ -23,14 +24,15 @@ export class MapViewComponent implements AfterViewInit, OnChanges{
   ){}
 
   // TO DO's: 
-  // marcadores hobbies
   // botón nueva búsqueda
-  // cuadrar las opciones de los nuevos hobbies
+  // cuadrar checkboxes
+  // cambiar icono marcador
 
   @Input() checkedCategories: number[] = [];
 
   housingService: HousesService = inject(HousesService)
   hobbiesService: HobbiesService = inject(HobbiesService)
+  categoriesService: CategoriesService = inject(CategoriesService)
 
   ngAfterViewInit(): void {  if(!this.placesService.userLocation) throw Error("No hay placesService.userLocation")
   this.map = new Map({
@@ -52,6 +54,8 @@ export class MapViewComponent implements AfterViewInit, OnChanges{
   }
 
   ngOnChanges(changes: SimpleChanges): void {
+    var categoryList = this.categoriesService.categories
+
     console.log(this.checkedCategories);
     if(!this.placesService.userLocation) throw Error("No hay placesService.userLocation")
 
@@ -67,7 +71,7 @@ export class MapViewComponent implements AfterViewInit, OnChanges{
                 .setHTML(`
                           <img style="max-width: 100%; border-radius: 50 px" src="${infoLocation.image}">
                           <h2>${infoLocation.name}</h2>`);
-            new Marker({ color: 'yellow' })
+            new Marker({ element: this.createCustomMarkerElement(categoryList[categoryId].url) })
                 .setLngLat(infoLocation.direction)
                 .setPopup(popup)
                 .addTo(this.map);
@@ -77,7 +81,7 @@ export class MapViewComponent implements AfterViewInit, OnChanges{
                 .setHTML(`
                           <img style="max-width: 100%; border-radius: 50 px" src="${this.housingService.houses[i].image}">
                           <h2>Casa de ${this.housingService.houses[i].contact}</h2>
-                          <p>Contacto: ${this.housingService.houses[i].phone} </p>`);
+                          <p>Contacto: ${this.housingService.houses[i].phone} </p><p> ${this.housingService.houses[i].precio}</p>`);
               new Marker({ color: 'blue' })
                 .setLngLat(this.housingService.houses[i].direction)
                 .setPopup(popup)
@@ -88,5 +92,14 @@ export class MapViewComponent implements AfterViewInit, OnChanges{
       }
     }
     this.mapService.setMap(this.map);
+  }
+
+  createCustomMarkerElement(iconUrl: string): HTMLElement {
+    const element = document.createElement('div');
+    element.className = 'custom-marker';
+    element.style.backgroundImage = `url(${iconUrl})`;
+    element.style.width = '128px';
+    element.style.height = '128px';
+    return element;
   }
 }
--- src/app/services/categories.service.ts
+++ src/app/services/categories.service.ts
@@ -9,11 +9,11 @@ export class CategoriesService {
   constructor() { }
 
   categories: { [id: number]: Category; } = {
-    0 : {name:"⚽  Sport"},
-    1 : {name:"👜 Shopping"},
-    2 : {name:"📖  Libraries"},
-    3 : {name:"🏋️  Gym"},
-    4 : {name:"🌳  Parks"},
-    5 : {name:"🏛️ Museums"}
+    0 : {name:"⚽  Sport", url:"https://cdn-icons-png.flaticon.com/128/3661/3661230.png"},
+    1 : {name:"👜 Shopping", url:"https://cdn-icons-png.flaticon.com/128/11792/11792501.png"},
+    2 : {name:"📖  Libraries", url:"https://cdn-icons-png.flaticon.com/128/1245/1245339.png"},
+    3 : {name:"🏋️  Gym", url:"https://cdn-icons-png.flaticon.com/128/12138/12138684.png"},
+    4 : {name:"🌳  Parks", url:"https://cdn-icons-png.flaticon.com/128/8325/8325733.png"},
+    5 : {name:"🏛️ Museums", url:"https://cdn-icons-png.flaticon.com/128/4615/4615742.png"}
  };
 }
